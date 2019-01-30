16d23889de7ba282b38fde09b6ab527c55b00625
TrueSuperMan <tync1989@gmail.com>
2019-01-30 12:47:05 +0600

    fix3

--- lesson_2/fibonacci.rb
+++ lesson_2/fibonacci.rb
@@ -1,6 +1,12 @@
 fib_arr = [0, 1]
+=begin
 num = 1
 loop do
   num >= 100 ? break : fib_arr << num
   num += fib_arr[-2]
 end
+=end
+while (result = fib_arr[-1] + fib_arr[-2]) < 100
+  fib_arr << result
+end
+print fib_arr
